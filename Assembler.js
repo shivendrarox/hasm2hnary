a8f31bc3607ccc533393eaee76de9e877c6060d2
Shivendra Singh <shivendrasingh@Shivendras-MacBook-Pro.local>
2022-12-14 21:14:02 +0530

    symbol table

--- Assembler.js
+++ Assembler.js
@@ -1,6 +1,9 @@
 //hasm2hnary: Hack ASM to Hack Binary assembler
 import  {dest,jump,comp}  from "./CodeModule.js"
-
+import { SymbolTable } from "./SymbolTable.js"
 // console.log(dest("M"))//001
 // console.log(jump("JEQ"))//010
 // console.log(comp("-A"))//11 00 11
+const symbolTable = new SymbolTable()
+
+console.log(symbolTable.addEntry("OPOPO",99))
---
+++ SymbolTable.js
@@ -0,0 +1,44 @@
+// SymbolTable Class
+export class SymbolTable{
+    #table
+    #lastAddress
+    constructor(){
+        this.#table = {
+            'R0':0,
+            'R1':1,
+            'R2':2,
+            'R3':3,
+            'R4':4,
+            'R5':5,
+            'R6':6,
+            'R7':7,
+            'R8':8,
+            'R9':9,
+            'R10':10,
+            'R11':11,
+            'R12':12,
+            'R13':13,
+            'R14':14,
+            'R15':15,
+            'SCREEN':16384,
+            'KBD':24576,
+            'SP':0,
+            'LCL':1,
+            'ARG':2,
+            'THIS':3,
+            'THAT':4,
+        }
+        this.#lastAddress = 16
+    }
+    addEntry(symbol,address){
+        this.#table[symbol] = parseInt(address)
+    }
+
+    contains(symbol){
+        return this.#table.hasOwnProperty(symbol)
+    }
+
+    GetAddress(symbol){
+        return this.#table[symbol]
+    }
+}
